237c2bb6dbbe663098929c8dd53d848219f42c4e
Miquel Lopez <miquellopezavila@gmail.com>
2024-02-09 22:02:08 +0100

    finish

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
 import Application from './application/Application';
-
+  
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Application />);
+const router = createBrowserRouter([
+  {
+    path: '/portfolio',
+    element: <Application />,
+  },
+]);
+
+root.render(<RouterProvider router={router} />);
